13516cc1298fad3790e2803f475e0ead71adb8e9
Anitha-krishnappa <anianitha7082@gmail.com>
2025-01-09 12:46:09 +0530

    Add vulnerable.js file with insecure eval usage

--- vulnerable.js
+++ vulnerable.js
@@ -1,11 +1,9 @@
 // vulnerable.js
 // This file demonstrates an insecure use of the `eval` function.
-
 function executeUserInput(input) {
     // Warning: Using eval to execute user input is dangerous and insecure!
     eval(input); // Vulnerable to injection attacks
 }
-
 // Example usage
 const userInput = "console.log('This is unsafe!');";
 executeUserInput(userInput);
